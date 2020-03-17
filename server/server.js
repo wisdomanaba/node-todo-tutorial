f5d58a0f4ea101cba623afdac767c9b79f595431
wizzydev <wisdomanaba83@gmail.com>
2020-03-17 13:43:58 +0100

    Frontend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -12,30 +12,39 @@ const PORT = process.env.PORT || 4500
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
-      const todo = new Todo({
-            text: req.body.text
-      })
+      const { text } = req.body
+      const todo = new Todo({text})
 
       todo.save()
       .then( doc => res.send(doc), e => res.status(400).send(e) )
 })
 
 app.get('/todos', (req, res) => {
-      Todo.find().then((todos) => {
+      Todo.find().then(todos => {
             res.send({todos})
       }, (e) => {
             res.status(400).send(e)
-            console.log('Unable to get file', e)
+            console.log('Unable to fetch data', e)
       })
+})
+
+app.get('/todos/:id', (req, res) => {
+      const { id } = req.params
+
+      !ObjectID.isValid(id) ? res.status(400).send() :
+
+      Todo.findById(id).then((todo) => {
+            !todo ? res.status(400).send() :
+            res.send({todo})
+      }).catch( e => res.status(400).send())
 })
 
 
 // User route
 
 app.post('/user', (req, res) => {
-      const user = new User({
-            email: req.body.email
-      })
+      const { email } = req.body
+      const user = new User({email})
 
       user.save()
       .then( doc => res.send(doc), e => res.status(400).send(e) )
@@ -54,7 +63,7 @@ module.exports = { app }
 
 
 
-
+// _AGb96%A_Ap4LUe
 
 
 // var otherTodo = new Todo({
